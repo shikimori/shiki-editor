01a34143b5ea693230a619d3c114af66cb6bd8a4
Andrey Sidorov <takandar@gmail.com>
2020-07-22 14:19:52 +0300

    add debounce decorator, sendRequest skeleton method

--- packages/shiki-editor/src/extensions/shiki_loader.js
+++ packages/shiki-editor/src/extensions/shiki_loader.js
@@ -1,9 +1,10 @@
 import pDefer from 'p-defer';
 import { Extension } from '../base';
+import debounce from 'debounce-decorator';
 
 export default class ShikiLoader extends Extension {
   data = {}
-  requests = {}
+  queue = null
 
   get name() {
     return 'shiki_loader';
@@ -12,9 +13,20 @@ export default class ShikiLoader extends Extension {
   fetch({ id, type }) {
     const deferred = pDefer();
 
-    requests[type] ||= {}
-    (requests[type][id] ||= []).push(deferred);
+    this.queue ||= {};
+    this.queue[type] ||= {};
+    (this.queue[type][id] ||= []).push(deferred);
+
+    this.sendRequest();
 
     return deferred.promise;
+  }
+
+  @debounce(50)
+  sendRequest() {
+    const { queue } = this;
+    this.queue = null;
+
+    console.log(queue);
   }
 }
