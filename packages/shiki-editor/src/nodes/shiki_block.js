5218b6dda291d1936fdc646d8735213e30774652
Andrey Sidorov <takandar@gmail.com>
2020-07-27 01:45:56 +0300

    cleanup shiki block

--- packages/shiki-editor/src/nodes/shiki_block.js
+++ packages/shiki-editor/src/nodes/shiki_block.js
@@ -1,18 +1,5 @@
 import { Node } from '../base';
-import { nodeInputRule } from '../commands';
 import { ShikiInlineView } from '../node_views';
-import { pasteRule } from '../commands';
-
-import {
-  SHIKI_BBCODE_LINK_REGEXP,
-  SHIKI_BBCODE_LINK_FULL_REGEXP,
-  SHIKI_BBCODE_IMAGE_REGEXP
-} from '../markdown/tokenizer/processors/shiki_inline';
-
-import {
-  parseShikiBasicMeta,
-  parseImageMeta
-} from '../markdown/tokenizer/bbcode_helpers';
 
 export default class ShikiBlock extends Node {
   get name() {
@@ -35,27 +22,6 @@ export default class ShikiBlock extends Node {
       group: 'block',
       draggable: false,
       selectable: false
-      // toDOM: node => {
-      //   if (node.attrs.text) {
-      //     return [
-      //       'span',
-      //       {
-      //         'data-attrs': JSON.stringify(node.attrs)
-      //       },
-      //       ['span', node.attrs.openBbcode],
-      //       ['span', 0],
-      //       ['span', node.attrs.closeBbcode]
-      //     ];
-      //   } else {
-      //     return [
-      //       'span',
-      //       {
-      //         'data-attrs': JSON.stringify(node.attrs)
-      //       },
-      //       node.attrs.bbcode
-      //     ];
-      //   }
-      // }
     };
   }
 
