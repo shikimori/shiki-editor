43cb04ece0ec57c6760979dd463e10e8f9d7c13e
Andrey Sidorov <takandar@gmail.com>
2020-07-27 19:40:46 +0300

    apply caching for shiki block

--- packages/shiki-editor/src/markdown/tokenizer/processors/shiki_block.js
+++ packages/shiki-editor/src/markdown/tokenizer/processors/shiki_block.js
@@ -1,5 +1,6 @@
 import Token from '../token';
 import { extractUntil } from '../helpers';
+import { CACHE, fixedType } from '../../../extensions/shiki_loader';
 
 export default function(state, openBbcode, closeBbcode, meta) {
   const inlineText = extractUntil(
@@ -19,23 +20,26 @@ export default function(state, openBbcode, closeBbcode, meta) {
 
   if (!text) { return; }
   const tokens = state.constructor.parse(text.trim());
+  const cache = CACHE?.[fixedType(meta.type)]?.[meta.id];
+  let token;
 
-  const tagMeta = {
-    type: meta.type,
-    id: meta.id,
-    openBbcode,
-    closeBbcode
-  };
-
-  let cache;
-
-  state.push(
-    new Token('shiki_block', null, tokens, {
-      ...tagMeta,
+  if (cache) {
+    token = new Token('link_block', null, tokens, {
+      url: cache.url,
+      id: meta.id,
+      type: meta.type
+    });
+  } else {
+    token = new Token('shiki_block', null, tokens, {
+      type: meta.type,
+      id: meta.id,
+      openBbcode,
+      closeBbcode,
       isLoading: cache === undefined,
       isError: cache !== undefined
-    })
-  );
+    });
+  }
 
+  state.push(token);
   state.next(openBbcode.length + text.length + closeBbcode.length);
 }
