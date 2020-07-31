a960354078f071c9693d7a74cf0d79b9a5e9b274
Andrey Sidorov <takandar@gmail.com>
2020-07-31 18:42:25 +0300

    do not parse italic style

--- packages/shiki-editor/src/marks/bold_inline.js
+++ packages/shiki-editor/src/marks/bold_inline.js
@@ -13,10 +13,12 @@ export default class BoldInline extends Mark {
       }, {
         tag: 'b',
         getAttrs: node => node.style.fontWeight !== 'normal' && null
-      }, {
-        style: 'font-weight',
-        getAttrs: value => /^(bold(er)?|[5-9]\d{2,})$/.test(value) && null
-      }],
+      }
+      // {
+      //   style: 'font-weight',
+      //   getAttrs: value => /^(bold(er)?|[5-9]\d{2,})$/.test(value) && null
+      // }
+      ],
       toDOM: () => ['strong', 0]
     };
   }
