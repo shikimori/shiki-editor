832ede049e6615520ecb75f7a2b5a18797661b7b
Andrey Sidorov <takandar@gmail.com>
2020-08-02 19:07:25 +0300

    do not break in ensureOnlyStyle when node has no style attribute

--- packages/shiki-editor/src/utils/ensure_only_style.js
+++ packages/shiki-editor/src/utils/ensure_only_style.js
@@ -1,9 +1,9 @@
 export default function ensureOnlyStyle(node, styleName) {
   const attributes = node
     .getAttribute('style')
-    .split(';')
-    .filter(v => v);
+    ?.split(';')
+    ?.filter(v => v);
 
-  return attributes.length === 1 &&
+  return attributes && attributes.length === 1 &&
     attributes[0].split(':')[0] === styleName;
 }
