cf2acf9efb18ecb37514dc67b45eb323b35236ab
Andrey Sidorov <takandar@gmail.com>
2020-08-24 20:27:06 +0300

    restore scroll position on mobile and on long texts

--- packages/shiki-editor/src/extensions/shiki_search.js
+++ packages/shiki-editor/src/extensions/shiki_search.js
@@ -1,13 +1,14 @@
 import { bind } from 'shiki-decorators';
 
 import { Extension } from '../base';
-import { getShikiLoader } from '../utils';
+import { getShikiLoader, scrollTop } from '../utils';
 
 export default class ShikiSearch extends Extension {
   editor = null
 
   _cancel = null
   _pick = null
+  scrollY = 0
 
   get name() {
     return 'shiki_search';
@@ -25,36 +26,19 @@ export default class ShikiSearch extends Extension {
 
   searchOpen(editor) {
     this.editor = editor;
+    this.scrollY = scrollTop();
 
-    this.stub();
+    this._stub();
     this.globalSearch.focus();
-  }
-
-  stub() {
-    this._cancel = this.globalSearch.cancel;
-    this._pick = this.globalSearch.pick;
-
-    this.globalSearch.isStubbedSearchMode = true;
-    this.globalSearch.cancel = this.searchClose;
-    this.globalSearch.pick = this.searchPick;
-  }
-
-  unstub() {
-    this.globalSearch.isStubbedSearchMode = false;
-    this.globalSearch.cancel = this._cancel;
-    this.globalSearch.pick = this._pick;
-
-    this._cancel = null;
-    this._pick = null;
-    this._isIndexDisabled = null;
   }
 
   @bind
   searchClose() {
-    this.unstub();
+    this._unstub();
     this.globalSearch.cancel();
 
     this.editor.focus();
+    window.scrollTo(0, this.scrollY);
   }
 
   @bind
@@ -77,5 +61,24 @@ export default class ShikiSearch extends Extension {
 
     dispatch(state.tr.replaceWith(selection.from, selection.to, editorNode));
     getShikiLoader(this.editor).addToCache(attrs.type, attrs.id, attrs, true);
+  }
+
+  _stub() {
+    this._cancel = this.globalSearch.cancel;
+    this._pick = this.globalSearch.pick;
+
+    this.globalSearch.isStubbedSearchMode = true;
+    this.globalSearch.cancel = this.searchClose;
+    this.globalSearch.pick = this.searchPick;
+  }
+
+  _unstub() {
+    this.globalSearch.isStubbedSearchMode = false;
+    this.globalSearch.cancel = this._cancel;
+    this.globalSearch.pick = this._pick;
+
+    this._cancel = null;
+    this._pick = null;
+    this._isIndexDisabled = null;
   }
 }
