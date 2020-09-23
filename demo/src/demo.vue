e3deddeed137451cee73cc156e9ce07ef8b95212
Andrey Sidorov <takandar@gmail.com>
2020-09-24 00:36:44 +0300

    misc

--- demo/src/demo.vue
+++ demo/src/demo.vue
@@ -60,8 +60,10 @@ import { throttle, debounce } from '../../packages/shiki-decorators';
 import ShikiUploader from '../../packages/shiki-uploader';
 
 const TEXT_2 = `
-aaaa ||bbbb|| \`test\` [b]bold[/b]
-[spoiler_block]test[/spoiler_block]
+[spoiler_block]
+> aaaa ||bbbb|| \`test\` [b][u][s]b[i]ol[/i]d[/s][/u][/b]
+test
+[/spoiler_block]
 `.trim();
 
 export default {
