d1a2ed066c368f3e7908e65a44aa88930bd7ee27
Andrey Sidorov <takandar@gmail.com>
2020-10-20 21:53:39 +0300

    add mobile_detect to shiki_utils

--- packages/shiki-utils/index.js
+++ packages/shiki-utils/index.js
@@ -1,3 +1,6 @@
 export { default as flash } from './src/flash';
 export { default as ShikiRequest } from './src/shiki_request';
 export { default as RequestId } from './src/request_id';
+import { mobileDetect, isMobile, isPhone, isTablet } from './src/mobile_detect';
+
+export { mobileDetect, isMobile, isPhone, isTablet };
