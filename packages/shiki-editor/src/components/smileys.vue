a3c56bb611162b1febfc658e3e32df7d31615542
Andrey Sidorov <takandar@gmail.com>
2020-10-20 22:34:02 +0300

    working on mobile smileys

--- packages/shiki-editor/src/components/smileys.vue
+++ packages/shiki-editor/src/components/smileys.vue
@@ -6,8 +6,12 @@
       :key-code='27'
       @success='close'
     />
-    <div ref='container' class='b-tip b-tip--large b-tip--no_border smileys'>
-      <div data-popper-arrow />
+    <div
+      ref='container'
+      class='smileys'
+      :class='containerCssClass'
+    >
+      <div v-if='!isMobile' data-popper-arrow />
       <div
         v-if='smileysHTML'
         class='inner'
@@ -16,12 +20,15 @@
       />
       <div v-else class='b-ajax' />
     </div>
-    <div class='shade' @click='close' />
+    <div v-if='!isMobile' class='shade' @click='close' />
   </div>
 </template>
 
 <script>
 import Keypress from 'vue-keypress';
+import { isMobile } from 'shiki-utils';
+import { disablePageScroll, enablePageScroll } from 'scroll-lock';
+
 // import { createPopper } from '@popperjs/core';
 import { createPopper } from '@popperjs/core/lib/popper-lite';
 import preventOverflow from '@popperjs/core/lib/modifiers/preventOverflow';
@@ -42,6 +49,18 @@ export default {
     popup: null,
     smileysHTML: null
   }),
+  computed: {
+    isMobile() {
+      return isMobile();
+    },
+    containerCssClass() {
+      if (this.isMobile) {
+        return 'fixed';
+      } else {
+        return 'b-tip b-tip--large b-tip--no_border';
+      }
+    }
+  },
   watch: {
     isEnabled() {
       if (this.isEnabled) {
@@ -64,6 +83,32 @@ export default {
   },
   methods: {
     show() {
+      if (this.isMobile) {
+        disablePageScroll();
+      } else {
+        this.showPopup();
+      }
+
+      if (!this.smileysHTML) {
+        this.fetch();
+      }
+    },
+    cleanup() {
+      if (this.popup) {
+        this.popup.destroy();
+        this.popup = null;
+      }
+    },
+    async fetch() {
+      const { data } = await this.shikiRequest.get('smileys');
+      this.smileysHTML = data.replace(/src="\//g, `src="${this.shikiRequest.origin}/`);
+
+      if (this.popup) {
+        await this.$nextTick();
+        this.popup.update();
+      }
+    },
+    showPopup() {
       this.popup = createPopper(
         this.$parent.$refs[this.targetRef][0].$el,
         this.$refs.container,
@@ -78,23 +123,12 @@ export default {
           }]
         }
       );
-      if (!this.smileysHTML) {
-        this.fetch();
+    },
+    async close() {
+      if (this.isMobile) {
+        enablePageScroll();
       }
-    },
-    cleanup() {
-      if (this.popup) {
-        this.popup.destroy();
-        this.popup = null;
-      }
-    },
-    async fetch() {
-      const { data } = await this.shikiRequest.get('smileys');
-      this.smileysHTML = data.replace(/src="\//g, `src="${this.shikiRequest.origin}/`);
-      await this.$nextTick();
-      this.popup.update();
-    },
-    close() {
+
       this.$emit('toggle');
     },
     select({ target }) {
@@ -113,42 +147,54 @@ $padding-horizontal: 10px
 $padding-vertical: 8px
 
 .smileys
-  padding: 0
   background: #fff
-  font-size: 13px
-  position: relative
   z-index: 40
 
-  +lte_ipad
-    width: calc(100vw - #{$padding-horizontal * 2})
-    max-width: 492px
-    min-height: 238px
-
-  +gte_laptop
-    min-height: 472px
-    width: 492px
-
-  /deep/ .smiley
-    cursor: pointer
-    margin-right: 7px
-    margin-bottom: 10px
-    outline: 2px solid transparent
-    transition: outline .15s
-
-    +gte_laptop
-      &:hover
-        outline: 2px solid var(--link-hover-color, #dd5202)
-
-    &:active
-      outline: 2px solid var(--link-active-color, #ff0202)
-
-  .inner
-    overflow-y: auto
-    overscroll-behavior: none
-    padding: $padding-vertical $padding-horizontal
+  &.b-tip
+    padding: 0
+    position: relative
 
     +lte_ipad
-      max-height: calc(100vh - 98px)
+      width: calc(100vw - #{$padding-horizontal * 2})
+      max-width: 492px
+      min-height: 238px
+
+    +gte_laptop
+      min-height: 472px
+      width: 492px
+
+    .inner
+      padding: $padding-vertical $padding-horizontal
+
+      +lte_ipad
+        max-height: calc(100vh - 98px)
+
+  &.fixed
+    height: 100%
+    left: 0
+    position: fixed
+    top: 0
+    width: 100%
+
+  .inner
+    max-height: 100%
+    overflow-y: auto
+    overscroll-behavior: none
+    padding: 16px
+
+/deep/ .smiley
+  cursor: pointer
+  margin-right: 7px
+  margin-bottom: 10px
+  outline: 2px solid transparent
+  transition: outline .15s
+
+  +gte_laptop
+    &:hover
+      outline: 2px solid var(--link-hover-color, #dd5202)
+
+  &:active
+    outline: 2px solid var(--link-active-color, #ff0202)
 
 .b-ajax
   width: calc(100% - #{$padding-horizontal * 2})
