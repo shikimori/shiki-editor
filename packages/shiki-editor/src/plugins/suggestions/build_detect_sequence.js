e8ed69b0da4c8879c5a8dfed468096ad1f8de460
Andrey Sidorov <takandar@gmail.com>
2020-11-05 23:40:06 +0300

    drop autocomplete on third space in the match

--- packages/shiki-editor/src/plugins/suggestions/build_detect_sequence.js
+++ packages/shiki-editor/src/plugins/suggestions/build_detect_sequence.js
@@ -25,7 +25,9 @@ export default function buildDetectSequence({
     const textTo = $position.end();
     const text = $position.doc.textBetween(textFrom, textTo, '\0', '\0');
 
-    let match = text.endsWith('  ') ? null : regexp.exec(text);
+    let match = text.endsWith('  ') || text.split(' ').length > 3 ?
+      null :
+      regexp.exec(text);
     let position;
 
     while (match !== null) {
