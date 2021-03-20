7a11305c0464a27155235a6a122efa21bfe92763
Andrey Sidorov <takandar@gmail.com>
2021-03-20 20:23:18 +0300

    smart replacement for inline commands

--- src/components/utils/insert_at_caret.js
+++ src/components/utils/insert_at_caret.js
@@ -1,4 +1,6 @@
-export default async function insertAtCaret(app, prefix, postfix, filler) {
+const INLINE_REPLACEMENT_REGEXP = /\n(.)/g;
+
+export default async function insertAtCaret(app, prefix, postfix, isInline, filler) {
   const textarea = app.$refs.textarea;
   const content = app.editorContent;
 
@@ -12,8 +14,8 @@ export default async function insertAtCaret(app, prefix, postfix, filler) {
 
   } else if (
     textarea.selectionStart ||
-    (textarea.selectionStart === '0') ||
-    (textarea.selectionStart === 0)
+      (textarea.selectionStart === '0') ||
+      (textarea.selectionStart === 0)
   ) {
     const startPos = textarea.selectionStart;
     const endPos = textarea.selectionEnd;
@@ -21,6 +23,15 @@ export default async function insertAtCaret(app, prefix, postfix, filler) {
     let selectedText = content.substring(startPos, endPos);
     selectedText = ((selectedText === '') && filler ? filler : selectedText);
 
+    if (
+      isInline && prefix && postfix &&
+        selectedText.match(INLINE_REPLACEMENT_REGEXP)
+    ) {
+      selectedText = selectedText.replace(
+        INLINE_REPLACEMENT_REGEXP,
+        `${postfix}\n${prefix}$1`
+      );
+    }
     app.editorContent = content.substring(0, startPos) +
       prefix + selectedText + postfix +
       content.substring(endPos, content.length);
--- src/components/utils/source_command.js
+++ src/components/utils/source_command.js
@@ -3,19 +3,27 @@ import insertAtCaret from './insert_at_caret';
 export default function sourceCommand(app, type, value) {
   switch (type) {
     case 'bold':
-      insertAtCaret(app, '[b]', '[/b]');
+      insertAtCaret(app, '[b]', '[/b]', true);
       break;
 
     case 'italic':
-      insertAtCaret(app, '[i]', '[/i]');
+      insertAtCaret(app, '[i]', '[/i]', true);
       break;
 
     case 'underline':
-      insertAtCaret(app, '[u]', '[/u]');
+      insertAtCaret(app, '[u]', '[/u]', true);
       break;
 
     case 'strike':
-      insertAtCaret(app, '[s]', '[/s]');
+      insertAtCaret(app, '[s]', '[/s]', true);
+      break;
+
+    case 'spoiler_inline':
+      insertAtCaret(app, '||', '||', true);
+      break;
+
+    case 'code_inline':
+      insertAtCaret(app, '`', '`', true);
       break;
 
     case 'smiley':
