505d19bb36a73256382782e56e0224383a3e6bf8
Andrey Sidorov <takandar@gmail.com>
2021-03-20 21:29:16 +0300

    remove support of IE<=9

--- src/components/utils/insert_at_caret.js
+++ src/components/utils/insert_at_caret.js
@@ -4,53 +4,37 @@ export default async function insertAtCaret(app, prefix, postfix, isInline, fill
   const textarea = app.$refs.textarea;
   const content = app.editorContent;
 
-  if (document.selection) {
-    textarea.focus();
-    const sel = document.selection.createRange();
-    sel.text = prefix +
-      (((sel.text === '') && filler ? filler : sel.text)) +
-      postfix;
-    textarea.focus();
+  const startPos = textarea.selectionStart;
+  const endPos = textarea.selectionEnd;
 
-  } else if (
-    textarea.selectionStart ||
-      (textarea.selectionStart === '0') ||
-      (textarea.selectionStart === 0)
+  const { scrollTop } = textarea;
+
+  let selectedText = content.substring(startPos, endPos);
+  selectedText = ((selectedText === '') && filler ? filler : selectedText);
+
+  if (
+    isInline && prefix && postfix &&
+      selectedText.match(INLINE_REPLACEMENT_REGEXP)
   ) {
-    const startPos = textarea.selectionStart;
-    const endPos = textarea.selectionEnd;
-    const { scrollTop } = textarea;
-    let selectedText = content.substring(startPos, endPos);
-    selectedText = ((selectedText === '') && filler ? filler : selectedText);
-
-    if (
-      isInline && prefix && postfix &&
-        selectedText.match(INLINE_REPLACEMENT_REGEXP)
-    ) {
-      selectedText = selectedText.replace(
-        INLINE_REPLACEMENT_REGEXP,
-        `${postfix}\n${prefix}$1`
-      );
-    }
-    app.editorContent = content.substring(0, startPos) +
-      prefix + selectedText + postfix +
-      content.substring(endPos, content.length);
-
-    app.focus();
-    await app.$nextTick();
-
-    if (selectedText.length || !prefix.length) {
-      textarea.selectionEnd = startPos + prefix.length +
-        selectedText.length + postfix.length;
-      textarea.selectionStart = textarea.selectionEnd;
-    } else {
-      textarea.selectionStart = startPos + prefix.length;
-      textarea.selectionEnd = textarea.selectionStart;
-    }
-    textarea.scrollTop = scrollTop;
-
-  } else {
-    app.editorContent += prefix + postfix;
-    app.focus();
+    selectedText = selectedText.replace(
+      INLINE_REPLACEMENT_REGEXP,
+      `${postfix}\n${prefix}$1`
+    );
   }
+  app.editorContent = content.substring(0, startPos) +
+    prefix + selectedText + postfix +
+    content.substring(endPos, content.length);
+
+  app.focus();
+  await app.$nextTick();
+
+  if (selectedText.length || !prefix.length) {
+    textarea.selectionEnd = startPos + prefix.length +
+      selectedText.length + postfix.length;
+    textarea.selectionStart = textarea.selectionEnd;
+  } else {
+    textarea.selectionStart = startPos + prefix.length;
+    textarea.selectionEnd = textarea.selectionStart;
+  }
+  textarea.scrollTop = scrollTop;
 }
